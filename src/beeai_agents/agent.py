5d5fa654a9523220cd1aa6050199711255934ff7
Chintan Soni <ichintansoni@gmail.com>
2025-08-29 19:13:12 +0530

    chore(deps): bump beeai sdk to 0.3.2
    Signed-off-by: Chintan Soni <ichintansoni@gmail.com>

--- src/beeai_agents/agent.py
+++ src/beeai_agents/agent.py
@@ -5,19 +5,22 @@ from a2a.types import (
 )
 from a2a.utils.message import get_message_text
 from beeai_sdk.server import Server
-from beeai_sdk.server.context import Context
+from beeai_sdk.server.context import RunContext
 from beeai_sdk.a2a.types import AgentMessage
 
 server = Server()
 
+
 @server.agent()
-async def example_agent(input: Message, context: Context):
+async def example_agent(input: Message, context: RunContext):
     """Polite agent that greets the user"""
     hello_template: str = os.getenv("HELLO_TEMPLATE", "Ciao %s!")
     yield AgentMessage(text=hello_template % get_message_text(input))
 
+
 def run():
-    server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
+    server.run(host=os.getenv("HOST", "127.0.0.1"),
+               port=int(os.getenv("PORT", 8000)))
 
 
 if __name__ == "__main__":
