bf14c98a3671652b95be2c5ab83e5c04d77bfa11
Tomas Weiss <tomas.weiss2@gmail.com>
2025-09-03 09:32:10 +0200

    chore: change base image
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- src/beeai_agents/agent.py
+++ src/beeai_agents/agent.py
@@ -10,17 +10,14 @@ from beeai_sdk.a2a.types import AgentMessage
 
 server = Server()
 
-
 @server.agent()
 async def example_agent(input: Message, context: RunContext):
     """Polite agent that greets the user"""
     hello_template: str = os.getenv("HELLO_TEMPLATE", "Ciao %s!")
     yield AgentMessage(text=hello_template % get_message_text(input))
 
-
 def run():
-    server.run(host=os.getenv("HOST", "127.0.0.1"),
-               port=int(os.getenv("PORT", 8000)))
+    server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
 
 
 if __name__ == "__main__":
