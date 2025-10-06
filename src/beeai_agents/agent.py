2f9892dcf0733e64fb3182e52a62b5b87e8263bd
Jan Pokorný <JenomPokorny@gmail.com>
2025-10-06 11:18:16 +0200

    feat: add watchfiles option
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- src/beeai_agents/agent.py
+++ src/beeai_agents/agent.py
@@ -17,7 +17,10 @@ async def example_agent(input: Message, context: RunContext):
     yield AgentMessage(text=hello_template % get_message_text(input))
 
 def run():
-    server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
+    try:
+        server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
